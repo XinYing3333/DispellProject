3be9b877c4733b44e33d0a5545c94f23f1bf73b1
XKFK <love.kyngh.ky@gmail.com>
2025-09-16 11:19:12 +0800

    documents arrange and clear redundant

--- Assets/Scripts/InteractPoint/InteractionEffect.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class InteractionEffect : MonoBehaviour
-{
-    public abstract void ExecuteEffect(InteractionPoint interactionPoint, Spell spell, InteractionTriggerType triggerType);
-}
--- Assets/Scripts/InteractPoint/InteractionTriggerType.cs
+++
@@ -1,6 +0,0 @@
-public enum InteractionTriggerType
-{
-    OnEnter,  // 當物件進入時觸發
-    OnStay,
-    OnExit    // 當物件離開時觸發
-}
